e819dc953c2f98e6e1f49cda3b3371930a4c9faf
ThiagoA20 <thiagohca2019@gmail.com>
2022-09-12 15:28:13 -0300

    warp perspective

--- warp_perspective.py
+++ warp_perspective.py
@@ -1,4 +1,16 @@
 import cv2 as cv
+import numpy as np
 
-cv.filter
-'assets/cards'
+img = cv.imread("assets/cards.jpg")
+
+width, height = 250,350
+pts1 = np.float32([[111,219], [287,188], [154,482], [352,440]])
+pts2 = np.float32([[0,0], [width,0], [0,height], [width, height]])
+matrix = cv.getPerspectiveTransform(pts1, pts2)
+imgOutput = cv.warpPerspective(img, matrix, (width, height))
+
+cv.imshow("Image", img)
+cv.imshow("Output", imgOutput)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
